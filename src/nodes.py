99929cc5c2e925ecdf0f7a01f229c20f4bd219a8
s0lst1ce <thithib.cohergne@gmail.com>
2020-03-22 13:31:33 +0100

    finished node and link obj

--- src/nodes.py
+++ src/nodes.py
@@ -1,12 +1,14 @@
 import pygame as pg
 from settings import *
 
-surf_infected = pg.Surface((NODE_RADIUS, NODE_RADIUS), color=RED)
-surf_sain = pg.Surface((NODE_RADIUS, NODE_RADIUS), color=BLUE)
+surf_infected = pg.Surface((NODE_RADIUS, NODE_RADIUS))
+surf_infected.fill(RED)
+surf_sain = pg.Surface((NODE_RADIUS, NODE_RADIUS))
+surf_sain.fill(BLUE)
 
 node_surfs = {
-    True: surf_sain,
-    False: surf_infected,
+    False: surf_sain,
+    True: surf_infected,
 }
 
 
@@ -23,4 +25,4 @@ class Node:
         self.surf = node_surfs[self.infected]
 
     def draw(self, dest):
-        dest.blit(self.surf, self.center.x, self.center.y)
+        dest.blit(self.surf, (self.center.x, self.center.y))
