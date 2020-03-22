efdd867825ce51b08d0df6bbc8f46cc3fe02d179
s0lst1ce <thithib.cohergne@gmail.com>
2020-03-22 12:56:34 +0100

    basic layout

--- src/main.py
+++ src/main.py
@@ -1,1 +1,36 @@
 import pygame as pgm
+from settings import *
+
+#init pygame
+pg.init()
+window = pg.display.set_mode((WIDTH, HEIGHT))
+game_surf = pg.Surface((WIDTH, HEIGHT))
+game_surf.fill(WHITE)
+clock = pg.time.Clock()
+running = True
+
+
+def start():
+    """populate the game_surf"""
+    pass
+
+
+def update():
+    pass
+
+def events():
+    global running
+
+    pressed_keys = pg.key.get_pressed()
+    events = pg.event.get()
+    for event in events:
+        if event.type == pg.QUIT pr pressed_keys[pg.K_ESCAPE]:
+            running = False
+
+
+def render():
+    pass
+
+
+def main_loop():
+    pass
