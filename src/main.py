a3b62491e4566d55257a681af5aeedc7b5c2e433
s0lst1ce <thithib.cohergne@gmail.com>
2020-03-22 12:59:33 +0100

    progress

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-import pygame as pgm
+import pygame as pg
 from settings import *
 
-#init pygame
+# init pygame
 pg.init()
 window = pg.display.set_mode((WIDTH, HEIGHT))
 game_surf = pg.Surface((WIDTH, HEIGHT))
@@ -12,11 +12,12 @@ running = True
 
 def start():
     """populate the game_surf"""
-    pass
+    global game_surf
 
 
 def update():
     pass
+
 
 def events():
     global running
@@ -24,13 +25,25 @@ def events():
     pressed_keys = pg.key.get_pressed()
     events = pg.event.get()
     for event in events:
-        if event.type == pg.QUIT pr pressed_keys[pg.K_ESCAPE]:
+        if event.type == pg.QUIT or pressed_keys[pg.K_ESCAPE]:
             running = False
 
 
 def render():
-    pass
+    global window
+    global game_surf
+
+    window.blit(game_surf, (0, 0))
+    pg.display.update()
 
 
 def main_loop():
-    pass
+    while running:
+        clock.tick(60)
+        events()
+        update()
+        render()
+
+
+start()
+main_loop()
