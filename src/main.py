b25401d862d655d1aa4757dfb02a95af956e982f
Kadir Aksoy <kursatkadir014@gmail.com>
2020-03-22 15:58:46 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pygame as pg
+from math import sin
 from settings import *
 from nodes import Node
 from link import Link
@@ -6,8 +7,7 @@ from link import Link
 # init pygame
 pg.init()
 window = pg.display.set_mode((WIDTH, HEIGHT))
-game_surf = pg.Surface((WIDTH, HEIGHT))
-game_surf.fill(WHITE)
+game_surf = pg.Surface((WIDTH, HEIGHT), pg.SRCALPHA).convert_alpha()
 clock = pg.time.Clock()
 running = True
 
@@ -18,6 +18,8 @@ link1 = Link(node1, node2)
 # this dict is useful because we need to draw the links before the nodes or it will look wrong (line over circle)
 entities = {"nodes": [node1, node2], "links": [link1]}
 
+# increasing variable for the animation of background
+background_increase = 0
 
 def start():
     """populate the game_surf"""
@@ -41,6 +43,15 @@ def events():
 def render():
     global window
     global game_surf
+    global background_increase
+
+    window.fill((52, 171, 235))
+    background_increase -= SPEED
+    a = 0
+    for y in range((HEIGHT//RES)+1):
+        a+=1
+        for x in range((WIDTH//RES)+1):
+            pg.draw.circle(window, (140, 215, 255), (x*RES, y*RES), (sin(x+a+background_increase)+2)*1.5)
 
     # drawing game content
     for link in entities["links"]:
